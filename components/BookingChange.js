91169e1b66372ef5d16c7682c8aafec73fa8614a
Prince Marecha <prince@auratechnologies.co.zw>
2022-09-07 11:38:42 +0200

    header refix

--- components/BookingChange.js
+++ components/BookingChange.js
@@ -1,114 +1,115 @@
-import React from 'react'
+import React from "react";
 import Head from "next/head";
 import Footer from "../components/footer";
-import Header from "../components/Header";
-import GetSadza from '../scripts/add'
+import Header from "../components/header";
+import GetSadza from "../scripts/add";
 
 const BookingChange = () => {
   return (
     <div>
       <Head>
         <link
-                rel="stylesheet"
-                href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
-            />
-            <link
-                rel="stylesheet"
-                href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
-            />
-            <link
-                rel="stylesheet"
-                href="https://cdn.jsdelivr.net/npm/tw-elements/dist/css/index.min.css"
-            />
-            <script async src="https://cdn.tailwindcss.com"></script>
-            <script async src="./../scripts/add.js"></script>
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/tw-elements/dist/css/index.min.css"
+        />
+        <script async src="https://cdn.tailwindcss.com"></script>
+        <script async src="./../scripts/add.js"></script>
       </Head>
 
-      <Header/>
-      
-      <div>
-        <form className='m-8'>
-          <div className='grid grid-cols-1'>
-            <h1 className='text-2xl'>Change Your Booking</h1>
-            <div className='my-8'>
-                <h1 className='text-lg'>Booking Information</h1>
-                <div className='grid grid-cols-1'>
-                    <div className='grid grid-cols-2'>
-                        <div className='col-span-1 grid grid-cols-1 m-4'>
-                            <label htmlFor='reference'>Reference</label>
-                            <input type="text"/>
-                        </div>
-                        <div className='col-span-1 grid grid-cols-1  m-4'>
-                            <label htmlFor='c-reference'>Client-Reference</label>
-                            <input type="text"/>
-                        </div>
-                        <div className='col-span-1 grid grid-cols-1  m-4'>
-                            <label htmlFor='c-reference'>Creation Date</label>
-                            <input type="date"/>
-                        </div>
-                        <div className='col-span-1 grid grid-cols-1  m-4'>
-                            <label htmlFor='c-reference'>Booking Confirmed</label>
-                            <input type="checkBox" value="CONFIRMED" defaultChecked/>
-                        </div>
-                        
-                    </div>
-                    <div className='grid grid-cols-2'>
-                        <div className='col-span-1 grid grid-cols-1 m-4'>
-                            <label htmlFor='reference'>Creation User</label>
-                            <input type="text"/>
-                        </div>
-                    </div>
-                    <div className='grid grid-cols-2'>
-                        <div className='col-span-1 grid grid-cols-1 m-4'>
-                            <label htmlFor='reference'>Holder&apos;s Name</label>
-                            <input type="text"/>
-                        </div>
-                        <div className='col-span-1 grid grid-cols-1 m-4'>
-                            <label htmlFor='reference'>Holder&apos;s Surname</label>
-                            <input type="text"/>
-                        </div>
-                    </div>
-                </div>
-            <h1 className='text-lg mt-4'>Hotel Information</h1>
-            <div className='grid grid-cols-1'>
-                    <div className='grid grid-cols-2'>
-                        <div className='col-span-1 grid grid-cols-1 m-4'>
-                            <label htmlFor='checkin'>Check-In-Date</label>
-                            <input type="date" name='checkin'/>
-                        </div>
+      <Header />
 
-                        <div className='col-span-1 grid grid-cols-1  m-4'>
-                            <label htmlFor='checkOut'>Check-Out-Date</label>
-                            <input type="date" name='checkOut'/>
-                        </div>
-                        <div className='col-span-1 grid grid-cols-1 m-4'>
-                            <label htmlFor='hotel-name'>Hotel Name</label>
-                            <input type="text" name='hotel-name'/>
-                        </div>
-                        <div className='col-span-1 grid grid-cols-1 m-4'>
-                            <label htmlFor='hotel-code'>Hotel Code</label>
-                            <input type="text" name='hotel-code'/>
-                        </div>
-                    </div>
+      <div>
+        <form className="m-8">
+          <div className="grid grid-cols-1">
+            <h1 className="text-2xl">Change Your Booking</h1>
+            <div className="my-8">
+              <h1 className="text-lg">Booking Information</h1>
+              <div className="grid grid-cols-1">
+                <div className="grid grid-cols-2">
+                  <div className="col-span-1 grid grid-cols-1 m-4">
+                    <label htmlFor="reference">Reference</label>
+                    <input type="text" />
+                  </div>
+                  <div className="col-span-1 grid grid-cols-1  m-4">
+                    <label htmlFor="c-reference">Client-Reference</label>
+                    <input type="text" />
+                  </div>
+                  <div className="col-span-1 grid grid-cols-1  m-4">
+                    <label htmlFor="c-reference">Creation Date</label>
+                    <input type="date" />
+                  </div>
+                  <div className="col-span-1 grid grid-cols-1  m-4">
+                    <label htmlFor="c-reference">Booking Confirmed</label>
+                    <input type="checkBox" value="CONFIRMED" defaultChecked />
+                  </div>
+                </div>
+                <div className="grid grid-cols-2">
+                  <div className="col-span-1 grid grid-cols-1 m-4">
+                    <label htmlFor="reference">Creation User</label>
+                    <input type="text" />
+                  </div>
+                </div>
+                <div className="grid grid-cols-2">
+                  <div className="col-span-1 grid grid-cols-1 m-4">
+                    <label htmlFor="reference">Holder&apos;s Name</label>
+                    <input type="text" />
+                  </div>
+                  <div className="col-span-1 grid grid-cols-1 m-4">
+                    <label htmlFor="reference">Holder&apos;s Surname</label>
+                    <input type="text" />
+                  </div>
+                </div>
+              </div>
+              <h1 className="text-lg mt-4">Hotel Information</h1>
+              <div className="grid grid-cols-1">
+                <div className="grid grid-cols-2">
+                  <div className="col-span-1 grid grid-cols-1 m-4">
+                    <label htmlFor="checkin">Check-In-Date</label>
+                    <input type="date" name="checkin" />
+                  </div>
+
+                  <div className="col-span-1 grid grid-cols-1  m-4">
+                    <label htmlFor="checkOut">Check-Out-Date</label>
+                    <input type="date" name="checkOut" />
+                  </div>
+                  <div className="col-span-1 grid grid-cols-1 m-4">
+                    <label htmlFor="hotel-name">Hotel Name</label>
+                    <input type="text" name="hotel-name" />
+                  </div>
+                  <div className="col-span-1 grid grid-cols-1 m-4">
+                    <label htmlFor="hotel-code">Hotel Code</label>
+                    <input type="text" name="hotel-code" />
+                  </div>
+                </div>
+              </div>
+              <h1 className="text-lg mt-4">Room Information</h1>
+              <div className="grid grid-cols-1">
+                <button type="button" id="add_room" onClick={GetSadza}>
+                  Button
+                </button>
+                <div className="grid grid-cols-1 " id="roomOptions">
+                  <div id="unitDiv"></div>
+                </div>
+              </div>
             </div>
-            <h1 className='text-lg mt-4'>Room Information</h1>
-            <div className='grid grid-cols-1'>
-            <button type="button" id="add_room" onClick={GetSadza}>Button</button>
-                    <div className='grid grid-cols-1 ' id="roomOptions">
-                       <div id='unitDiv'></div>      
-                    </div>
-            </div>
-            </div>
-          </div>  
+          </div>
         </form>
       </div>
-      <Footer/>
+      <Footer />
       <script
-          async
-          src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js"
-        ></script>
+        async
+        src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js"
+      ></script>
     </div>
-  )
-}
+  );
+};
 
-export default BookingChange
+export default BookingChange;
