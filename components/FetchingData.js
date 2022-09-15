5a7d3168cdf15659f3e551f1593dad25bf2f1440
Prince Marecha <prince@auratechnologies.co.zw>
2022-09-15 09:04:53 +0200

    fix axios

--- components/FetchingData.js
+++ components/FetchingData.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
-import axios from 'axios'
+//import axios from 'axios'
 
 const FetchingData = () => {
     const [posts, setPosts] = useState([])
--- components/WithAxios.js
+++ components/WithAxios.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"
-import axios from 'axios'
+//import axios from 'axios'
 import CryptoJS from 'crypto-js'
 
 
