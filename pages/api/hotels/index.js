ebc6e2967c76bf8b355c6a03dbe65720032e72ac
Prince Marecha <prince@auratechnologies.co.zw>
2022-09-23 16:28:12 +0200

    hotels api

--- pages/api/hotels/index.js
+++ pages/api/hotels/index.js
@@ -1,12 +1,13 @@
 import nc from "next-connect"
 import db from "../../../utils/db";
 import Hotel from "../../../models/hotels";
+import Work from "../../../models/testMongo";
 
 const handler = nc();
 
 handler.get (async(req, res) =>{
     await db.connect();
-    const hotels = await Hotel.find({});
+    const hotels = await Work.find({});
     await db.disconnect();
     res.send(hotels);
 })
