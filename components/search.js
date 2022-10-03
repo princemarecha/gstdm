ddc0ad0e50666bf00992f49de8f6441774870aa4
Prince Marecha <prince@auratechnologies.co.zw>
2022-10-03 15:59:48 +0200

    search working

--- components/search.js
+++ components/search.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component, useContext } from 'react';
 import Tab from './tab';
+import { hotelContext } from '../Helper/Context';
 
-class Search extends Component {
-    render() {
-        return (
-            <div className="bg-white py-6 sm:py-8 lg:py-12 background-image">
-  <div className="max-w-screen-2xl px-4 md:px-8 mx-auto">
-  <Tab/>
+
+const Search = () => {
+
+  return (
+    <div className="bg-white py-6 sm:py-8 lg:py-12 background-image">
+    <div className="max-w-screen-2xl px-4 md:px-8 mx-auto">
+    <Tab/>
+    </div>
   </div>
-</div>
-        )
-    }
+  )
 }
-export default Search;
+
+export default Search
