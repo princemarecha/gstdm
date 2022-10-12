b61f76508b4e5bbdefde04f32bc48829177242dd
Shakani-Chiradza <shakanichiradza@gmail.com>
2022-10-12 10:49:52 +0200

    Commit by user 3

--- next.config.js
+++ next.config.js
@@ -4,8 +4,26 @@ const nextConfig = {
   swcMinify: true,
 }
 
+const { createProxyMiddleware } = require("http-proxy-middleware")
+
+
 module.exports = {nextConfig,
   images: {
     domains: ['photos.hotelbeds.com']
+  }
+}
+
+module.exports = () => {
+  const rewrites = () => {
+    return [
+      {
+        source: '/hotel-api/1.0/hotels',
+        destination: 'https://api.test.hotelbeds.com/hotel-api/1.0/hotels'
+      },
+    ];
+  };
+
+  return {
+    rewrites,
   }
 }
