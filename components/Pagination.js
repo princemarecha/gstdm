36a9a299b8347be030b3642c3cca0b28dbc2b56e
Prince Marecha <pmanstylz@gmail.com>
2022-11-16 12:04:46 +0200

    tabIndex (Pagination.js)

--- components/Pagination.js
+++ components/Pagination.js
@@ -8,7 +8,7 @@ const Pagination = () => {
       <ul className="flex list-style-none">
         <li className="page-item disabled"><a
             className="page-link relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-500 pointer-events-none focus:shadow-none"
-            href="#" tabindex="-1" aria-disabled="true">Previous</a></li>
+            href="#" tabiIndex="-1" aria-disabled="true">Previous</a></li>
         <li className="page-item"><a
             className="page-link relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
             href="#">1</a></li>
