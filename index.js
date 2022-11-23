de5bd62c8e4a8b6eb08247eaeed8f579b6995491
Nyasha Mashinya <admin@auragrp.com>
2022-11-23 10:45:00 +0000

    Updated index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import React, { Component, useState } from 'react';
 import Head from 'next/head';
-import Footer from './../components/footer';
-import Header from './../components/header';
-import Search from './../components/search';
-import Listings from './../components/listings';
-import { hotelContext } from '../Helper/Context';
-import { searchContext } from '../Helper/Context';
+import Footer from './components/footer';
+import Header from './components/header';
+import Search from './components/search';
+import Listings from './components/listings';
+import { hotelContext } from './Helper/Context';
+import { searchContext } from './Helper/Context';
 
 
 
