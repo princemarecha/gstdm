cbdb23517b4bfb9c4144fffc44fb34f0d5891693
Prince Marecha <pmanstylz@gmail.com>
2022-12-02 14:19:56 +0200

    delete font cdn

--- pages/paginationTest.js
+++ pages/paginationTest.js
@@ -42,7 +42,7 @@ const [status, setStatus] = useState([]);
     <div>
     <div>
         <Head>
-          <link rel="stylesheet" href="http://fonts.cdnfonts.com/css/roboto" />
+          
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" />
           <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tw-elements/dist/css/index.min.css" />
